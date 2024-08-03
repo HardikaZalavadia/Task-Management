096cc80069092f7e75b5160d8e1c436e6061b4cc
Hardika Zalavadia <hardika.youcan@gmail.com>
2024-08-03 17:36:49 +0530

    @Repository added

--- taskManagment/src/main/java/dev/hardika/taskManagment/taskManagment/repository/TaskRepository.java
+++ taskManagment/src/main/java/dev/hardika/taskManagment/taskManagment/repository/TaskRepository.java
@@ -2,10 +2,12 @@ package dev.hardika.taskManagment.taskManagment.repository;
 
 import dev.hardika.taskManagment.taskManagment.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUserId(Long userId);
     List<Task> findByStatusAndUserId(String status, Long userId);
--- taskManagment/src/main/java/dev/hardika/taskManagment/taskManagment/repository/UserRepository.java
+++ taskManagment/src/main/java/dev/hardika/taskManagment/taskManagment/repository/UserRepository.java
@@ -2,10 +2,11 @@ package dev.hardika.taskManagment.taskManagment.repository;
 
 import dev.hardika.taskManagment.taskManagment.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 }
